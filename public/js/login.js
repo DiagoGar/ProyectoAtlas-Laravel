4ea028dfb29db9d7bdad981decc6d1c63e746fe4
Diago Garcia <diagogh6@gmail.com>
2023-08-02 22:46:13 -0300

    first commit

---
+++ public/js/login.js
@@ -0,0 +1,6 @@
+document.querySelector(".login").addEventListener("click", ()=>{
+  document.querySelector(".login-showed").style.display="block"
+  document.querySelector(".container-login").style.display="block"
+  document.querySelector("body").style.background='#000000'
+  document.querySelector("body").style.transition='all 2s'
+})
