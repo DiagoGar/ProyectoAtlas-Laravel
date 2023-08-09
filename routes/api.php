4cb1c3d3f56933250b5627abd4497160848b256d
Diago Garcia <diagogh6@gmail.com>
2023-08-09 15:44:19 -0300

    configuro rutas para almacen

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlmacenController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,9 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
   Route::get('user-profile', [AuthController::class, 'userProfile']);
   Route::post('logout', [AuthController::class, 'logout']);
 });
+
+Route::get('/almacen', [AlmacenController::class, 'index']);
+Route::post('/almacen', [AlmacenController::class, 'store']);
+Route::get('/almacen/{id}', [AlmacenController::class, 'show']);
+Route::put('/almacen/{id}', [AlmacenController::class, 'update']);
+Route::delete('/almacen/{id}', [AlmacenController::class, 'destroy']);
