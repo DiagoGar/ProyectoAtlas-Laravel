ba175b7315969b2b93e0963a3f3876285ce4a9c1
Diago Garcia <diagogh6@gmail.com>
2023-08-15 12:15:22 -0300

    saco un echo en el metodo login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -21,7 +21,6 @@ class AuthController extends Controller
 
     public function register(Request $request){
         //validacion de datos
-        echo "hola";
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
