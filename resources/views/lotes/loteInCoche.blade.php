6556d38999a24a9da2a5140889609ad4751d02f4
Diago Garcia <diagogh6@gmail.com>
2023-11-12 17:52:17 -0300

    interfaz grafica para ver los lotes en cocher

--- resources/views/lotes/loteInCoche.blade.php
+++ resources/views/lotes/loteInCoche.blade.php
@@ -1,16 +1,35 @@
-@extends('layouts.main')
+@extends('layouts.actions')
 
 @section('title', 'Lotes en camion')
 
-@section('form')
-  <table class="mt-64">
-    @foreach ($data as $item)
-      <tr>
-        <td>{{$item['patente']}}</td>
-      </tr>
-      <tr>
-        <td>{{$item['idLotes']}}</td>
-      </tr>
-      @endforeach
+@section('object', 'Lotes en camion')
+
+@section("table")
+<div class="relative overflow-x-auto">
+  <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 text-center">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+              <th scope="col" class="px-6 py-3">
+                  Patente del camion
+              </th>
+              <th scope="col" class="px-6 py-3">
+                  Lote
+            </th>
+          </tr>
+      </thead>
+      <tbody>
+@foreach($data as $item)
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
+            <td class="p-5">{{ $item['patente']}}</td>
+            <td class="p-5">id: {{ $item['idLotes']}}</td>
+          </tr>
+          @endforeach
+        </tbody>
     </table>
+</div>
+
+<div class="text-center my-5">
+<a href="/mapData">Ir al mapa</a>
+</div>
+
 @endsection
